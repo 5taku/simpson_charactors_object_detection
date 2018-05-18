edef35fddd246b02c7e9ef75f6045194cf6ff25e
5taku <ksulki@gmail.com>
2018-05-19 06:19:12 +0900

    오류 있는 bboax 제거 로직 추가

--- simpson_csv.py
+++ simpson_csv.py
@@ -13,23 +13,24 @@ result = pd.DataFrame(columns=('filename','width','height','class','xmin','ymin'
 for i in range(len(bbox_datas_df)):
     bbox_data = bbox_datas_df.ix[i]
 
-    filename = bbox_data['filename']
-    filename = filename.replace('characters','simpson')
-    filename = filename.replace('simpson2', 'simpson')
+    if (bbox_data['xmax'] - bbox_data['xmin'] > 30 and bbox_data['ymax'] - bbox_data['ymin'] > 30):
+        filename = bbox_data['filename']
+        filename = filename.replace('characters','simpson')
+        filename = filename.replace('simpson2', 'simpson')
 
-    im = Image.open(filename)
-    width,height = im.size
+        im = Image.open(filename)
+        width,height = im.size
 
-    result.loc[i] = ({
-      'filename':filename,
-      'width':width,
-      'height':height,
-      'class':bbox_data['class'],
-      'xmin':bbox_data['xmin'],
-      'ymin':bbox_data['ymin'],
-      'xmax':bbox_data['xmax'],
-      'ymax':bbox_data['ymax']
-    })
+        result.loc[i] = ({
+          'filename':filename,
+          'width':width,
+          'height':height,
+          'class':bbox_data['class'],
+          'xmin':bbox_data['xmin'],
+          'ymin':bbox_data['ymin'],
+          'xmax':bbox_data['xmax'],
+          'ymax':bbox_data['ymax']
+        })
 
 rate = len(result)/8.0
 result = result.sample(frac=1)
